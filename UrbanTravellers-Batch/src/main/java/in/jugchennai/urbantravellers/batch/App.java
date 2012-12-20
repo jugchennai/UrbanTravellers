8e6f947a8780581e531c0aa28a73846ec7c5bad0
rajmahendra <rajmahendra@rajlaptop.(none)>
2012-12-20 10:58:23 +0530

    Initial checkin

---
+++ UrbanTravellers-Batch/src/main/java/in/jugchennai/urbantravellers/batch/App.java
@@ -0,0 +1,13 @@
+package in.jugchennai.urbantravellers.batch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ UrbanTravellers-JPA/src/main/java/in/jugchennai/urbantravellersjpa/App.java
@@ -0,0 +1,13 @@
+package in.jugchennai.urbantravellersjpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
