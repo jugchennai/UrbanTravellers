fbe6c19f239d58fcceeba54e36a5f03a7f451ff3
rajmahendra <rajmahendra@gmail.com>
2013-01-22 17:14:54 +0530

    chaged the websocket name

--- UrbanTravellers-Web/src/main/java/in/jugchennai/urbantravellers/websocket/GameBoardSocket.java
+++ UrbanTravellers-Web/src/main/java/in/jugchennai/urbantravellers/websocket/GameBoardSocket.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2013 JUGChennai.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package in.jugchennai.urbantravellers.websocket;
 
@@ -26,9 +37,10 @@ import org.codehaus.jettison.json.JSONObject;
 
 /**
  *
- * @author prasannakumar
+ * @author Prasanna Kumar <prassee.sathian@gmail.com>
+ * @author Rajmahendra Hegde <rajmahendra@gmail.com>
  */
-@WebSocketEndpoint(value = "/utRollDice",
+@WebSocketEndpoint(value = "/UTGameSocket",
 encoders = {DataEncoder.class},
 decoders = {DataDecoder.class},
 factory = GameBoardEndpointFactory.class)
