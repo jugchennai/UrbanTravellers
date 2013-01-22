dc062450eeea174797892e1b05f6d97af76ee6f4
rajmahendra <rajmahendra@gmail.com>
2013-01-22 17:33:41 +0530

    updated comments and license

--- UrbanTravellers-Web/src/main/java/in/jugchennai/urbantravellers/game/GameBoardConfig.java
+++ UrbanTravellers-Web/src/main/java/in/jugchennai/urbantravellers/game/GameBoardConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 JUGChennai.
+ * Copyright 2013 JUGChennai.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ package in.jugchennai.urbantravellers.game;
 
 /**
  * POJO to hold constants expected by GameBoard
- * @author prasannakumar
+ * @author Prasanna Kumar <prassee.sathian@gmail.com>
  */
 public class GameBoardConfig {
 
