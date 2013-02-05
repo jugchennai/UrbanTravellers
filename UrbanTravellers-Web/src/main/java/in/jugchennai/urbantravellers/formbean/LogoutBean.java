ce2a7b2097966632c9c3f9f8fa3fc51643fe026e
mgosemath <mgosemath@gmail.com>
2013-02-04 23:58:45 -0800

    Create LogoutBean.java
    Session Destroyed on logout

---
+++ UrbanTravellers-Web/src/main/java/in/jugchennai/urbantravellers/formbean/LogoutBean.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013 JUGChennai.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package in.jugchennai.urbantravellers.formbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author MahiRaj Gosemath
+ */
+@ManagedBean
+@RequestScoped
+public class LogoutBean {
+    
+  public String logout() {
+    String result="/faces/login.xhtml?faces-redirect=true";
+
+    FacesContext context = FacesContext.getCurrentInstance();
+    context.getExternalContext().invalidateSession();
+    
+    return result;
+  }
+}
+
