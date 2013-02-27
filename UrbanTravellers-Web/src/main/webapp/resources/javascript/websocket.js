f6c4074700498234b5cc4dca01728b73989fe2b3
Mahesh Gosemath <mgosemath@gmail.com>
2013-02-27 20:40:29 +0530

    Update UrbanTravellers-Web/src/main/webapp/resources/javascript/websocket.js

--- UrbanTravellers-Web/src/main/webapp/resources/javascript/websocket.js
+++ UrbanTravellers-Web/src/main/webapp/resources/javascript/websocket.js
@@ -13,5 +13,77 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+  var dice,name;
+	var wsUri = "ws://localhost:8080/UrbanTravellers-Web/UTGameDashSocket";
+	var websocket = new WebSocket(wsUri);
+        websocket.onopen = function(evt) { onOpen(evt) };
+        websocket.onmessage = function(evt) { onMessage(evt) };
+        websocket.onerror = function(evt) { onError(evt) };  
+            
+	function init() {
+            output = document.getElementById("output");    
+        }
+			
+        function say_hello() {
 
+            name = document.getElementById("createGame:nameField").value;                                
+            var json = JSON.stringify({
+                "type" : "createGame",     //type refers to message type to be sent eg. createGame on Create Game Event will be roll on Dice Roll event
+                "gameId" :name,
+                "playerName": "mgosemath"
+            });
+
+            websocket.send(json);
+            writeToScreen("SENT: " +json);
+            console.log("Msg Sent"+json);   
+            turnmodule();         	
+        }
+
+            function roll()
+            {
+                //code to update players position
+            }
+
+        function onOpen(evt) {
+            alert("Connected");
+            writeToScreen("WELCOME !!!! you have been now placed in the game board !!! ");
+        }
+
+        function onError(evt) {
+            writeToScreen('<span style="color: red;">ERROR:</span> ' + evt);
+        }
+
+        function updateScore(obj) {
+                if(obj.player === "Pras") {
+                    prasscore.innerHTML = obj.position
+                } else if (obj.player === "Raj") {
+                    rajscore.innerHTML = obj.position
+            }
+        }
+
+        function onMessage(evt) {
+            alert("Response received");
+            var ab =evt.data;
+            var json = ab;
+            obj = JSON.parse(json);
+            console.log("Msg Recieving");
+            console.log(ab);
+            writeToScreen("RECEIVED: " + evt.data);
+            //updateScore(obj);
+            //conditions depending on type of message received
+        }
+
+        function writeToScreen(message) {
+            var pre = document.createElement("p");
+            pre.style.wordWrap = "break-word";
+            pre.innerHTML = message;
+            output.appendChild(pre);
+        }
+        
+        //to hide createGame form and to display roll dice form
+        function turnmodule()
+        {	
+            document.getElementById("createGame").style.display="none";
+            document.getElementById("rollDice").style.display="block";
+        }
 
