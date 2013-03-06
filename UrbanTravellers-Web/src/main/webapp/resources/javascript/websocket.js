b4f995dc0a851b1977c0d2c2ff7d3e6b59ea1e69
Mahesh Gosemath <mgosemath@gmail.com>
2013-03-06 17:40:01 +0530

    Update websocket.js

--- UrbanTravellers-Web/src/main/webapp/resources/javascript/websocket.js
+++ UrbanTravellers-Web/src/main/webapp/resources/javascript/websocket.js
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-  	var dice,name;
-	var wsUri = "ws://localhost:8080/UrbanTravellers-Web/UTGameDashSocket";
-	var websocket = new WebSocket(wsUri);
-        websocket.onopen = function(evt) { onOpen(evt) };
-        websocket.onmessage = function(evt) { onMessage(evt) };
-        websocket.onerror = function(evt) { onError(evt) };  
+    var dice,name, username;
+    var wsUri = "ws://localhost:8080/UrbanTravellers-Web/UTGameDashSocket";
+    var websocket = new WebSocket(wsUri);
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    websocket.onerror = function(evt) { onError(evt) };  
             
 	function init() {
             output = document.getElementById("output");    
@@ -26,11 +26,13 @@
 			
         function say_hello() {
 
-            name = document.getElementById("createGame:nameField").value;                                
+            name = document.getElementById("createGame:nameField").value;           
+			username=document.getElementById("createGame:userName").value;
             var json = JSON.stringify({
-                "type" : "createGame",    //type of request for processing createGame will trigger createGame code and roll_dice will trigger roll_dice code
+                "type" : "createGame",
+				"status" : "pageLoaded",
                 "gameId" :name,
-                "playerName": "mgosemath"
+                "playerName": username
             });
 
             websocket.send(json);
@@ -39,14 +41,28 @@
             turnmodule();       	
         }
 
-            function roll()
-            {
-
-            }
+        function rollDice()
+        {
+			//gameId=document.getElementById("rollDice:gameId").value;
+			name = document.getElementById("rollDice:userName").value;
+            dice = Math.floor((Math.random()*6)+1);
+            //document.getElementById("rollDice:diceField").value = dice;
+                
+            var json = JSON.stringify({
+				"type" : "roll",
+                    "gameId" : "game1",
+                    "playerName": name,
+                    "diceValue": dice
+                });
+                
+                websocket.send(json);
+                writeToScreen("SENT: " +json);
+        }
 
         function onOpen(evt) {
             alert("Connected");
             writeToScreen("WELCOME !!!! you have been now placed in the game board !!! ");
+			onConnected();
         }
 
         function onError(evt) {
@@ -65,15 +81,38 @@
             alert("Response received");
             var ab =evt.data;
             var json = JSON.parse(ab);
-            if(json.type==="createGame")   //list the newly created games as sidebar
+			if(json.type==="loadGames")
+			{
+				alert("Loading Game List");
+				if(json.status==="no_game")
+				{
+					var temp=document.createElement("newDiv");
+					temp.innerHTML="<div id='no_game' class='noGame'>"+
+                                "<p>No Active Game</p>"+
+                                "</div>";
+					document.getElementById("games").appendChild(temp);
+				}
+			}
+            if(json.type==="createGame"||json.status==="active")
             {
-                var temp=document.createElement("newDiv");
+		alert("Game Found.");
+		if(document.getElementById("no_game"))
+		{
+			document.getElementById("no_game").style.display="none";
+                }
+		var temp=document.createElement("newDiv");
                 temp.innerHTML="<div class='box'><form>"+
                                 "Name :"+json.gameId+"<br/>"+
                                 "Players :"+json.players+"<br/>"+
-                                "<input class='btn btn-primary' type='button' value='Join' /></form></div>";
+								"<input type='hidden' value='"+json.gameId+"' id='hid'><br/>"+
+                                "<input id='joinGame' class='btn btn-primary' type='button' onclick='join()' value='Join' /></form></div>";
                 document.getElementById("games").appendChild(temp);
+
             }
+	    if(json.type==="roll")
+	    {
+				//writeToScreen("RECEIVED: " + evt.data);
+	    }
             //obj = JSON.parse(json);
             //var some=JSON.parse(evt.data);
             //alert(some.gameId);
@@ -90,9 +129,34 @@
             output.appendChild(pre);
         }
 
-	//to hide the game create form and display roll dice form
         function turnmodule()
         {	
             document.getElementById("createGame").style.display="none";
             document.getElementById("rollDice").style.display="block";
         }
+		
+	function onConnected()
+	{
+		var json = JSON.stringify({
+                	"type" : "loadGames"
+        	});
+
+        	websocket.send(json);
+        	writeToScreen("SENT: " +json);
+        	console.log("Msg Sent"+json);
+	}
+		
+	function join()
+	{
+		name = document.getElementById("hid").value;     
+		username=document.getElementById("createGame:userName").value;
+            	var json = JSON.stringify({
+                	"type" : "gameJoined",
+                	"gameId" :name,
+                	"playerName": username
+            	});
+
+            	websocket.send(json);
+            	writeToScreen("SENT: " +json);
+		turnmodule();
+	}
